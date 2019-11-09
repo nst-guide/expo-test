e00f5f57a39b0d49f771860a3b4683c4656379c5
Kyle Barron <kylebarron2@gmail.com>
2019-11-08 19:03:57 -0500

    Use SafeAreaView around MapView

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import MapView from "react-native-maps";
-import { StyleSheet, View, Dimensions } from "react-native";
-import Geojson from './Geojson';
-MapView.Geojson = Geojson
+import { SafeAreaView, StyleSheet, View, Dimensions } from "react-native";
+import Geojson from "./Geojson";
+MapView.Geojson = Geojson;
+import MapData from "./assets/map_data/track.json";
+import { Provider as PaperProvider } from "react-native-paper";
 
 const myPlace = {
   type: "FeatureCollection",
@@ -18,19 +20,34 @@ const myPlace = {
   ]
 };
 
-export default class App extends React.Component {
+class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <MapView style={styles.mapStyle}>
-          <Geojson geojson={myPlace} />
+          <Geojson geojson={MapData} />
         </MapView>
       </View>
     );
   }
 }
 
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.safeAreaViewContainer}>
+        <PaperProvider>
+          <Main />
+        </PaperProvider>
+      </SafeAreaView>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
+  safeAreaViewContainer: {
+    flex: 1
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -38,6 +55,9 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   mapStyle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height
   }
