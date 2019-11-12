97b4b81c993a06e8e0266e247ab9d6695f9a70f7
Kyle Barron <kylebarron2@gmail.com>
2019-11-12 16:35:51 -0700

    Add more buttons to Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,13 +2,22 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Map } from "../components/Map";
 import DrawerButton from "../components/DrawerButton";
+import AddContentButton from "../components/AddContentButton";
+import FindWaypointsButton from "../components/FindWaypointsButton";
+import { Portal, Provider } from "react-native-paper";
 
+// TODO: Debug why adding <Portal> stops the FontAwesome icons from working
+// I think you need to use <Portal> to get the FAB group to render at the right place on top of other objects
 export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Map style={styles.map} />
         <DrawerButton onPress={() => this.props.navigation.toggleDrawer()} />
+        <AddContentButton onPress={() => console.log("add content")} />
+        {/* <FindWaypointsButton
+              onPress={() => console.log("Find waypoints")}
+            /> */}
       </View>
     );
   }
