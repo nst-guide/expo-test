8f0ec4a6e33670dafd80e1937c0bca4612c3efef
Kyle Barron <kylebarron2@gmail.com>
2019-11-14 13:20:32 -0700

    Simple example of expanding view

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { View } from "react-native";
-import { Appbar, Text } from "react-native-paper";
+import { Appbar, Text, Button } from "react-native-paper";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import ExpandingButton from "../components/ExpandingButton";
 
 class SettingsScreen extends React.Component {
+  state = {
+    isCollapsed: true
+  };
+
   static navigationOptions = {
     drawerLabel: "Settings",
     drawerIcon: ({ tintColor }) => <FontAwesome5 name="cog" color={tintColor} />
   };
+
+  handleClick() {
+    console.log(this.state)
+    console.log(this.state.isCollapsed)
+    console.log(!this.state.isCollapsed);
+    this.setState({ isCollapsed: !this.state.isCollapsed });
+  }
 
   render() {
     return (
@@ -20,6 +32,15 @@ class SettingsScreen extends React.Component {
           <Appbar.Content title="Settings" />
         </Appbar.Header>
         <Text>App settings</Text>
+        <Text>Here's an expanding button:</Text>
+        <Button
+          icon="camera"
+          mode="contained"
+          onPress={() => this.handleClick()}
+        >
+          Expand
+        </Button>
+        <ExpandingButton collapsed={this.state.isCollapsed} />
       </View>
     );
   }
