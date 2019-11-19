2811f0873adf1480230c534c848cf8143df87729
Kyle Barron <kylebarron2@gmail.com>
2019-11-19 13:52:29 -0700

    Update username and password validations

--- core/utils.js
+++ core/utils.js
@@ -9,12 +9,18 @@ export const emailValidator = (email) => {
 
 export const passwordValidator = (password) => {
   if (!password || password.length <= 0) return 'Password cannot be empty.';
+  if (password.length < 8) return 'Password must be at least 8 characters';
 
   return '';
 };
 
 export const usernameValidator = (username) => {
-  if (!username || username.length <= 0) return 'Name cannot be empty.';
+  const validSymbols = /^[a-zA-Z0-9-_~]+$/;
+
+  if (!username || username.length <= 0) return 'Username cannot be empty.';
+  if (!validSymbols.test(username)) {
+    return 'Username can contain only a-z, A-Z, 0-9, -, ~, and _.'
+  }
 
   return '';
 };
